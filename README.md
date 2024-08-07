c07e3617f05bf8ed67f54682cb050605de9235cf
jmaczan <jedrzejpawel@maczan.pl>
2024-08-07 18:07:10 +0200

    Build with CMakeLists.txt. Inference code in progress

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # yolov3-tiny-openvino
 YOLOv3-tiny with OpenVINO and OpenCV in C++
+
+Designed for a person recognition on a very low-end hardware like my good old Lenovo ThinkPad x230 <3
---
+++ inference.cpp
@@ -0,0 +1,16 @@
+#include <openvino/openvino.hpp>
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    ov::Core core;
+    
+    //char* model_path = argv[2];
+    //char* compile_target = argv[3]; // "CPU" or "GPU"
+    std::shared_ptr<ov::Model> model = core.read_model("/home/user/models/yolov3-tiny/public/yolo-v3-tiny-onnx/FP32/yolo-v3-tiny-onnx.xml"); // model_path
+    ov::CompiledModel compiled_model = core.compile_model(model, "CPU"); // compile_target
+    ov::InferRequest infer_request = compiled_model.create_infer_request();
+    ov::Tensor input_tensor = infer_request.get_input_tensor();
+    float* input_data = input_tensor.data<float>();
+}
